76a9d4ba918f2fc54b0fb02820a71e0be29461e0
AHMETTASDELEN <ahmettasdelen5499@gmail.com>
2022-05-31 20:24:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,76 @@
+﻿using Business.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+
+            //ProductManager productManager = new ProductManager(new EfProductDal());
+            //var result = productManager.GetAll();
+            //Console.WriteLine(result.Message);
+            //if (result.Success=false)
+            //{
+            //    foreach (var product in result.Data)
+            //    {
+            //        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+
+
+
+
+
+            //ProductManager productManager = new ProductManager
+            //    (new EfProductDal(),
+            //    new CategoryManager(new EfCategoryDal()));
+
+            //ProductManager productManager = new ProductManager(new EfProductDal());
+
+            //var result = productManager.GetAll();
+            //if (result.Success == true)
+            //{
+            //    foreach (var product in result.Data)
+            //    {
+            //        Console.WriteLine(product.ProductName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+
+            //var result = productManager.GetProductDetails();
+            //if (result.Success == true)
+            //{
+            //    foreach (var product in result.Data)
+            //    {
+            //        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+            //        Console.WriteLine(result.Message);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+        }
+
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(1, 50))
+            {
+                Console.WriteLine(product.ProductName /*+ "/" + product.CategoryName*/);
+                global::System.Console.WriteLine(result.Message);
+            }
+        }
+    }
+}
